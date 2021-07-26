58c675b55b8e77a30885edca14de27db8e920472
MPrabhanath Nair <MPrabhanath.Nair@genmills.com>
2021-07-26 20:52:03 +0530

    Adding class for card shuffling code

---
+++ Decks/Service Layer/CardShufling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Decks.Service_Layer
+{
+    public class CardShufling
+    {
+        static Random r = new Random();      
+        static public void Shuffle(int[] deck)
+        {
+            for (int n = deck.Length - 1; n > 0; --n)
+            {
+                int k = r.Next(n + 1);
+                int temp = deck[n];
+                deck[n] = deck[k];
+                deck[k] = temp;
+            }
+        }
+    }
+}
